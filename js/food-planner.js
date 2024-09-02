1a069d501032e81fccf24cd221a9f100c81df94d
ADedicatedPuppy <179887025+ADedicatedPuppy@users.noreply.github.com>
2024-09-01 20:24:08 -0400

    feat: basic list system

---
+++ js/food-planner.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+  dishInput = document.getElementById('dish-input');
+  dishContainer = document.getElementById('dish-container');
+  counterElement = document.getElementById('dish-counter');
+};
+let dishInput, dishContainer, counterElement;
+let counter = 0;
+
+const addDishInput = (event) => {
+  if(event.key == "Enter") {
+    addDishButton();
+  }
+}
+
+const addDishButton = () => {
+  if (dishInput.value) {
+    addDish(dishInput.value);
+  }
+
+  dishInput.value = '';
+};
+
+const addDish = (dishName) => {
+  let div = document.createElement('div');
+  div.className = 'card dish-card p-2 m-1';
+  div.innerText = dishName;
+
+  dishContainer.appendChild(div);
+
+  counter++;
+  counterElement.innerText = `Count: ${counter}`;
+};
